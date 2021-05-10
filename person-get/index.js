5a1d1226ab832fb48784425e59c95c2475613c6f
EricKuan <erickuan@fareastone.com.tw>
2021-05-10 12:00:51 +0800

    modify: 測試多加 log

--- person-get/index.js
+++ person-get/index.js
@@ -4,7 +4,8 @@ module.exports = async function (context, req, queryPerson ) {
         context.log("Persion not find!")
     }
     else{
-        context.log("Found ToDo item, Description=" + queryPerson.id);
+        context.log("Found user, id=" + queryPerson.id);
+        context.log("Found user, userName=" + queryPerson.name );
     }
     
     context.res = {
